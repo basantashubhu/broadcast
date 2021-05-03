dfc5062532d3e6202f3772331eeb531ae085828d
basantashubhu <callmebasanta44@gmail.com>
2021-05-03 20:32:26 +0545

    broadcast

---
+++ database/seeders/AgentsTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class AgentsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $password = md5('password@1');
+        DB::insert("INSERT INTO `lsv_agents` VALUES ('1', 'admin', 'first', 'last', 'admin', '$password', 'admin@admin.com', '1', '', '');");
+    }
+}
